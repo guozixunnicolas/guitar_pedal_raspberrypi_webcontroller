d33323e609ff75b842efad738a71d71dc85386f5
Kenneth Anderson <eucliwood98@gmail.com>
2020-01-22 20:42:41 +0800

    Removed old boiler code, changed to simple example

--- website/control/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-
-# Create your views here.
-
-def index(request):
-    return render(request, 'control/index.html')
