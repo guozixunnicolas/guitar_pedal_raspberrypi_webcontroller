5405eeddc1b09b529d0f6221fb822687234ba36c
Kenneth Anderson <eucliwood98@gmail.com>
2020-02-27 01:01:18 +0800

    WIP

--- website/frontend/src/index.js
+++ website/frontend/src/index.js
@@ -2,35 +2,94 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import io from 'socket.io-client';
 import './index.css';
+import Control from './components/ControlBar';
+import config from './config.json';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.socket = io(config.endpoint);
         this.state = {
-            connectedUsers: new Map(),
-            endpoint: 'http://localhost:5000'
+            controls: {
+                equalizer: {
+                    value: Array(8).fill(0),
+                    min: Array(8).fill(0),
+                    max: Array(8).fill(1),
+                    step: Array(8).fill(0.01)
+                },
+                gain: {
+                    value: 0,
+                    min: 0,
+                    max: 100,
+                    step: 1
+                },
+                reverb: {
+                    value: 0,
+                    min: 0,
+                    max: 100,
+                    step: 1
+                },
+                delay: {
+                    value: 0,
+                    min: 0,
+                    max: 100,
+                    step: 1
+                },
+                dry: {
+                    value: 0,
+                    min: 0,
+                    max: 100,
+                    step: 1
+                },
+                wet: {
+                    value: 0,
+                    min: 0,
+                    max: 100,
+                    step: 1
+                }
+            }
         }
     }
     setSocketListeners() {
-        const { endpoint } = this.state;
-        const socket = io(endpoint);
-        socket.on('user_connected', (user) => {
-            this.state.connectedUsers.set(user.id, user.audio_conf);
+        this.socket.on('user_connected', (data) => {
+            console.log(data)
         });
-        
-        socket.on('user_disconnected', (user) => {
-            this.state.connectedUsers.delete(user.id);
-        });
-        
     }
     componentDidMount() {
         this.setSocketListeners();
+    }
+    handleControlChange() {
+        this.socket.emit('set_control', {
+
+        });
+    }
+    generateControls() {
+        const controls = [];
+        const cur_controls = this.state.controls;
+        for(const key in cur_controls) {
+            if(key === 'equalizer') {
+                for(const field in cur_controls[key]) {
+                    //  Implement this
+                }
+            }
+            else {
+                controls.push(<Control min={cur_controls[key].min} max={cur_controls[key].max} value={cur_controls[key].value} label={key.toUpperCase()} className='equalizer'></Control>)
+            }
+        }
+        return controls;
+    }
+    render() {
+        return (
+            <div id='app'>
+                {this.generateControls()}
+            </div>
+        )
     }
 }
 
 // ========================================
 
 ReactDOM.render(
-    <body>Test</body>,
+    <App></App>,
     document.getElementById('root')
 );
