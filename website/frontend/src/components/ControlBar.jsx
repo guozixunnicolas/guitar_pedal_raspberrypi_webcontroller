5cf27d7d900235025135410f79654a95300862cb
Kenneth Anderson <eucliwood98@gmail.com>
2020-02-27 01:00:36 +0800

    Added ControlBar component along with its css style

--- website/frontend/src/components/ControlBar.jsx
+++ website/frontend/src/components/ControlBar.jsx
@@ -1,19 +1,51 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import '../css/control.css';
 
 class ControlBar extends React.Component {
-    handleChange() {
-        
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 0,
+            label: null,
+        };
     }
-    render() {
-        return(
-            <input type="range" onChange={this.handleChange()}></input>
+    handleSliderChange(event) {
+        this.setState({ value: event.target.value });
+    }
+    componentDidMount() {
+        this.setState({
+            value: this.props.value,
+            label: this.props.label
+        });
+    }
+    generateSlider() {
+        return (
+            <div style={{ marginTop: "20px", marginBottom: "20px" }}>
+                <span style={{ fontSize: "16px", marginBottom: "6px" }}>
+                    {" "}
+                    {this.state.label} ({this.state.value})
+                </span>
+                <input
+                    id={this.state.label}
+                    type="range"
+                    defaultValue="0"
+                    min={this.props.min}
+                    max={this.props.max}
+                    className={this.props.className}
+                    onChange={this.handleSliderChange.bind(this)}
+                    value={this.state.value}
+                    step="1"
+                />
+            </div>
         )
     }
-}
-
-ControlBar.PropTypes = {
-
+    render() {
+        return (
+            <div>
+                {this.generateSlider()}
+            </div>
+        )
+    }
 }
 
 export default ControlBar;
