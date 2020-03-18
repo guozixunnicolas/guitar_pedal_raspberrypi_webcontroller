59d7f8610b671ced6b4d17f71094f0c575a1a57b
Kenneth Anderson <eucliwood98@gmail.com>
2020-03-18 23:00:20 +0800

    Added the stream endpoint port, and some json attempt

--- website/backend/config.py
+++ website/backend/config.py
@@ -1,3 +1,4 @@
+import json
 #   Stream
 N_CHUNKS = 1
 
@@ -8,9 +9,14 @@ CHUNK = 1536
 BITS_PER_SAMPLE = 16
 API_ENDPOINT_PORT = 5000
 CLIENT_ENDPOINT_PORT = 3000
+STREAM_ENDPOINT_PORT = 8000
 
 default_control = {
     "gain": 10,
     "delay": 0,
     "reverb": 0
 }
+
+if __name__ == "__main__":
+    with open('../frontend/src/config.json', mode='r') as file:
+        print(json.load(file))
