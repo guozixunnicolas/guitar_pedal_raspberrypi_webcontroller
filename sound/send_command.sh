b529661db96a544758f80580a5566a94f370aba4
Guo_Zixun <legendzixun@gmail.com>
2020-03-27 16:32:56 +0800

    add sound processing

---
+++ sound/send_command.sh
@@ -0,0 +1,11 @@
+echo '0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.777 0 0.5 0.5 0.5 0.5 200 0.8 1 bang bang 1 bang;' | pdsend 15001 localhost
+
+#1-8 eq band value (0,1)
+#9 eq preamp (0, 1)
+#10 eq bypass (0 / 1)
+
+#11,12,13,14 reverb roomsize, damp, dry, wet
+
+#15, 16 delay time, val
+
+#16-20 record, tempo, clear, audio on/off, reverse
