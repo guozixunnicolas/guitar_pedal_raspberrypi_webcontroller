e0e67c5566d2e9b592bf8e1dec6d7d6d04a9864b
Kenneth Anderson <eucliwood98@gmail.com>
2020-03-31 20:32:49 +0800

    Added helper function to iterate specific filetypes files from given dir

--- website/backend/util.py
+++ website/backend/util.py
@@ -1,5 +1,6 @@
 import socket
 import random
+import os
 
 def unique_random_n_digits(digit: int, cur_digit: set()):
     start = 10**(digit - 1)
@@ -16,6 +17,15 @@ def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
+    
+def iterateFilesFromDir(path: str, file_type: str = None):
+    for root, _, files in os.walk(path):
+        for file in files:
+            if file_type is not None:
+                if file_type in file:
+                    yield os.path.join(root, file)
+            else:
+                yield os.path.join(root, file)
 
 if __name__ == "__main__":
     import timeit
