6cb704ee314769a791f8b8c72c94bd9215ab99c6
Kenneth Anderson <eucliwood98@gmail.com>
2020-04-21 01:11:11 +0800

    Implemented helper function for processing the list of not normalized prefixes.

--- website/backend/User.py
+++ website/backend/User.py
@@ -1,5 +1,9 @@
 import config
 import uuid
+import functools
+
+def key_is_not_in_list(word_list: list, key: str) -> bool:
+    return functools.reduce(lambda a, b: a and b, [_ not in key for _ in word_list])
 class User(object):
     def __init__(self, uid: str = None, request = None, port: int = None):
         self.id = uid if uid is not None else uuid.uuid4()
@@ -13,8 +17,22 @@ class User(object):
         }
     
     def audio_conf_as_pd_payload(self):
+        '''
+        Flatten the audio_conf as a string joined by " " and applying neccessary normalization
+        Eg: 
+        "equalizer": [1, 2, 3, 4, 5, 6, 7, 8],
+        "not normalized": 100,
+        "will be normalized by 100": 200
+        =>
+        1 2 3 4 5 6 7 8 100 2
+        '''
         assert 'equalizer' in self.audio_conf, 'Default config not found'
-        return " ".join([str(int_value) for int_value in [*[eq_val / config.EQUALIZER_NORMALIZED_CONST for eq_val in self.audio_conf["equalizer"]], *[val / config.CONTROLS_NORMALIZED_CONST if 'fixed' not in key else val for key, val in self.audio_conf.items() if not isinstance(val, list)]]])
+        return " ".join([
+            str(int_value) for int_value in [
+                *[eq_val / config.EQUALIZER_NORMALIZED_CONST for eq_val in self.audio_conf["equalizer"]], 
+                *[val / config.CONTROLS_NORMALIZED_CONST if key_is_not_in_list(config.NOT_NORMALIZED_CONST_PREFIX, key) else val for key, val in self.audio_conf.items() if not isinstance(val, list)]
+            ]
+        ])
 
 if __name__ == "__main__":
     user = User('123')
