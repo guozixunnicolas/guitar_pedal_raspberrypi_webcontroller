816a8f0d94da97289332126fdae588ecd21ea32c
Kenneth Anderson <eucliwood98@gmail.com>
2020-04-22 16:25:30 +0800

    updated auto start script

--- start.sh
+++ start.sh
@@ -1,1 +1,24 @@
-echo '0 1 2;' | pdsend 15001 localhost
+#!/bin/bash
+# pid list to kill
+pids=( )
+# define cleanup func
+cleanup() {
+	for pid in "${pids[@]}"; do
+		kill -0 "$pid" && kill "$pid" #kill process if running
+	done
+}
+# trap when EXIT signal is received
+trap cleanup EXIT TERM
+
+cd /home/pi/Documents/DIP
+source ./env/bin/activate
+cd website/backend
+python server.py & pids+=( "$!" )
+cd ..
+cd frontend
+npm start & pids+=( "$!" )
+ip=`hostname -I | cut -d' ' -f1`
+address="http://${ip}:3000"
+message="Running Audio Livestream Webserver on $address"
+echo $message
+wait
